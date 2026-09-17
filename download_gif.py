17fcaf17cbaf8a6771ebd8fb3b60f08d61e98f34
MathStrike <92716055+MathStrike@users.noreply.github.com>
2026-09-17 13:22:58 +0200

    ajout de tous les gis normaux

--- download_gif.py
+++ download_gif.py
@@ -7,7 +7,7 @@ import requests
 from PIL import Image, ImageSequence
 
 # Nombre de Pokémon à télécharger, à partir du n°1
-NB_POKEMON = 9
+NB_POKEMON = 649
 
 # Dossier de destination (doit correspondre à celui utilisé par l'appli web).
 # Ancré sur l'emplacement du script pour que ça marche peu importe d'où on le lance.
